9929c1e59435a71150eec6fc7c633b21f4eb6d4b
wout.van.brussel <wout.van.brussel@student.howest.be>
2025-01-19 18:58:05 +0100

    chapter 13

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -158,10 +158,12 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
+    // throw new Error('Failed to fetch invoice.');
+    return null;
   }
 }
 
