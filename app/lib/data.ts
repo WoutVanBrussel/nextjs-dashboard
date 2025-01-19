c948a7704bff1c615ff591ff0a04cc5b726928d4
wout.van.brussel <wout.van.brussel@student.howest.be>
2025-01-19 01:12:49 +0100

    chapter 7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -28,14 +28,14 @@ export async function fetchRevenue() {
   }
 }
 
-export async function fetchLatestInvoices() {
+export async function fetchLatestInvoices(amount: number) {
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 5`;
+      LIMIT ${amount}`;
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
